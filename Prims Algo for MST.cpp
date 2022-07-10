5d248b6d28096ee31ffbc28031eb89be84252627
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2022-07-10 22:12:29 +0530

    Create Prims Algo for MST.cpp

---
+++ Prims Algo for MST.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int N=3,m=6;
+	vector<pair<int,int> > adj[N]; 
+	adj[0].push_back({1,5});
+	adj[0].push_back({2,1});
+	adj[1].push_back({0,5});
+	adj[1].push_back({2,3});
+	adj[2].push_back({1,3});
+	adj[2].push_back({0,1});	
+    int parent[N]; 
+      
+    int key[N]; 
+      
+    bool mstSet[N]; 
+  
+    for (int i = 0; i < N; i++) 
+        key[i] = INT_MAX, mstSet[i] = false; 
+    
+    priority_queue< pair<int,int>, vector <pair<int,int>> , greater<pair<int,int>> > pq;
+
+    key[0] = 0; 
+    parent[0] = -1; 
+    pq.push({0, 0});
+
+    while(!pq.empty())
+    { 
+        int u = pq.top().second; 
+        pq.pop(); 
+        
+        mstSet[u] = true; 
+        
+        for (auto it : adj[u]) {
+            int v = it.first;
+            int weight = it.second;
+            if (mstSet[v] == false && weight < key[v]) {
+                parent[v] = u;
+	    key[v] = weight; 
+                pq.push({key[v], v});    
+            }
+        }
+            
+    } 
+    
+    for (int i = 1; i < N; i++) 
+        cout << parent[i] << " - " << i <<" \n"; 
+	return 0;
+}
